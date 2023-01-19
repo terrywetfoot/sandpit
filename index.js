3d78c8b807b107c4a25d481a981ec2dc6a8adbb1
terrywetfoot <abisset@posteo.net>
2023-01-19 13:40:31 +0000

    Fixed Empty Divs from being Owl Empty & Added Indentation
    <div> </div> In owl is now (:div '')
    
    Each child element indents, which is much nicer to eyeball!

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 //function to generate the owl string
 function generateOwl(doc) {
   let owl = '';
-
   //HELPER 1
   //function to generate tag name and attributes
   function generateTagAttr(element) {
-    console.log(element)
     let tag = element.tagName.toLowerCase();
     let attributes = element.getAttributeNames();
     owl += `(:${tag} `;
@@ -18,47 +16,122 @@ function generateOwl(doc) {
   //function to generate text content of the element
   function generateText(element) {
     let text = element.textContent.trim();
-    owl += " '" + text + "' " ;
+    owl += " '" + text + "'" ;
   }
   //HELPER 3
   //function to generate the closing tag
   function generateClosingTag() {
-      owl += `) \r `;
+      owl += `)`;
   }
-  ////////////////////////////////////////////////////////////////
-
+  //HELPER 4
+  //function to generate the tab forward 
+  function generateTabForward(indentLevel) {
+    for (let i = 0; i < indentLevel; i++) {
+    owl += `  `;
+    }
+  }
+  //HELPER 5
+  //function to generate the return
+  function generateReturn() {
+    owl += `\r`;
+  }
+  //HELPER 6
+  //function to check if the element is an empty div
+  function isEmptyDiv(element) {
+    return element.tagName === 'DIV' && element.childNodes.length === 0;
+  }
+  //HELPER 7
+  //function to check if the element is a text node
+  function isTextNode(element) {
+    return element.nodeType === Node.TEXT_NODE && element.textContent.trim() !== '';
+  }
   //The main function 
-  function metamorphose(element) {
+  function metamorphose(element, indentLevel) {
 
     if (element instanceof Element){
-      generateTagAttr(element);
+      generateReturn();
+      generateTabForward(indentLevel);
+      generateTagAttr(element)
 
+      //if empty div, generate the empty quote
+      if (isEmptyDiv(element)) {
+        generateText(element);
+      }
       //inner loop for child nodes
       for (const child of element.childNodes) {
-        if (child.nodeType === Node.TEXT_NODE && child.textContent.trim() !== "") {
+        //Children indent one level deeper   
+        indentLevel += 1;
+        if (isTextNode(child)) {
           generateText(child);
-        }
-        metamorphose(child)
+        } 
+        metamorphose(child, indentLevel);
+        //Bring back the indent level so we don't end up with a right angle triangle thing
+        //and instead have some sort of toblerone that's been nibbled on a bit
+        indentLevel -= 1;
       }
-
-      generateClosingTag(element);
-    }
-
-  }//end of metamorphose function
-
-  //WHERE THE THING ACTUALLY STARTS ////////////////////////
-  for (const child of doc.childNodes) {
-    metamorphose(child)
+    generateClosingTag(element);   
   }
-
-  return owl ;
+  }//end of metamorphose function
+  //WHERE THE THING ACTUALLY STARTS ////////////////////////
+  indentLevel = 0;
+  for (const child of doc.childNodes) {
+    metamorphose(child, indentLevel)
+  }
+return owl ;
 }
+
+
+
+
+
+
+
+test = `<!DOCTYPE html>
+                  <html>
+                    <head>
+                      <title>Page Title</title>
+                    </head>
+                    <body>
+                      <div id="thedivmaster">
+                        <div class="empty"></div>
+                        <div></div>
+                        <div id="thedivchef">
+                          <div></div>
+                          <div></div>
+                          <div></div>
+                          <div></div>
+                          <div>
+                            <div>
+                              <div>
+                                <div>
+                                  <div>
+                                    <div>
+                                      <div>
+                                        <div>
+                                          <div>  <input type="hidden" value="3" /> <p>some words that should be quite far forward? </p></div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <h1>This is a Heading</h1>
+                      <p id="paragidentifier">This </br> is a paragraph.</p>
+                      <p>This is another paragraph.</p>
+                      <p>This is a paragraph with <a href="https://www.w3schools.com">a link</a>.</p>
+                    </body>
+                  </html>`
 
 
 
 // Convert button
 $("#convert-button").on("click", function() { 
-    const input = $("#user-input").val().trim();
+    // const input = $("#user-input").val().trim();
+    const input = test;
     const parser = new DOMParser();
     const doc = parser.parseFromString(input, "text/html");
   
